a4b91aa79d7aca2174509379ad23f9320c7e14b2
mathias bigler <orchidevej@hotmail.com>
2017-11-14 12:26:27 +0100

    foundation

---
+++ src/main/java/FunctionLayer/LoginException.java
@@ -0,0 +1,12 @@
+package FunctionLayer;
+
+/**
+ * The purpose of LoginSampleException is to...
+ * @author kasper
+ */
+public class LoginException extends Exception {
+
+    public LoginException(String msg) {
+        super(msg);
+    }
+}
