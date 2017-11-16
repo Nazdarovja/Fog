8919e9bbd89937beb69eb7c4d08ff7c8b0466591
Alexander W. Hørsted-Andersen <awha86@gmail.com>
2017-11-16 12:15:19 +0100

    pom update

--- src/main/java/DataLayer/CustomerMapper.java
+++ src/main/java/DataLayer/CustomerMapper.java
@@ -4,16 +4,42 @@ import FunctionLayer.Customer;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  *
  * @author Alexander W. Hørsted-Andersen <awha86@gmail.com>
  */
 public class CustomerMapper {
+    
+    
+    public static void main(String[] args) throws Exception {
+        login("test", "test");
+    }
 
-    public static Customer login(String email, String password) throws SQLException {
-        return null;
+    public static Customer login(String email, String password) throws Exception {
+        Connection con = DBConnector.getConnection();
+        String SQL = "SELECT email,navn, efternavn, telefonNummer, adresse, postnummer, password FROM Customer "
+                + "WHERE email=? AND password=?";
+        PreparedStatement ps = con.prepareStatement(SQL);
+        ps.setString(1, email);
+        ps.setString(2, password);
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+
+            String mail = rs.getString("email");
+            String name = rs.getString("navn");
+            String surname = rs.getString("efternavn");
+            int phonenumber = rs.getInt("telefonNummer");
+            int address = rs.getInt("adresse");
+            int zipcode = rs.getInt("postnummer");
+//            String city = //methode til at hente fra anden table
+
+            Customer customer = new Customer(email, name, surname, phonenumber, surname, phonenumber, null);
+            return customer;
+        } else {
+            throw new Exception("could not validate user");
+        }
+
     }
 }
