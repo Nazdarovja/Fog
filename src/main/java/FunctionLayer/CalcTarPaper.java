eaa599cda80b86b740a09da7c22c8afb274c74df
Alexander W. Hørsted-Andersen <awha86@gmail.com>
2017-11-22 14:17:20 +0100

    work in progress

--- src/main/java/FunctionLayer/CalcTarPaper.java
+++ src/main/java/FunctionLayer/CalcTarPaper.java
@@ -12,11 +12,22 @@ package FunctionLayer;
 public class CalcTarPaper {  //milimeter
 
     public static OrderLine calcTarPaper(double length, double width, Product tarPaper, String roofType) {
-
+        
+        double overlay = 100;
         int tarPaperCounter = 0;
         double productLength = tarPaper.getLength();
-        double tarPaperLenNeeded = length * ((width) / 10000);  //pr meter width 
-
+        double productWidth = tarPaper.getWidth();
+        
+        //
+        double widthRollsNeeded = width / ((productWidth*10)-overlay);  
+        System.out.println("rolls needed (width): "+widthRollsNeeded);
+        
+        double tarPaperLenNeeded = length * (width / 10000);  //pr meter width 
+        System.out.println("DEBUG - tarPaperLenNeeded: "+tarPaperLenNeeded);
+        
+        double rowLengt = length / (productLength*10);
+        double res = widthRollsNeeded * rowLengt;
+        
         //tarPaper rolls needed
         while (tarPaperLenNeeded > 0) {
             tarPaperLenNeeded -= productLength;
@@ -24,15 +35,15 @@ public class CalcTarPaper {  //milimeter
         }
         
         //Pitched roof
-        if (roofType.equals("fladt")) {
-            tarPaperCounter = tarPaperCounter * 2;
+        if (roofType.equals("rejsning")) {
+            res = res * 2;
         }
 
         return new OrderLine(tarPaper, tarPaperCounter);
     }
 
     public static void main(String[] args) {
-        double length = 2500;
+        double length = 5000;
         double width = 4000;
         Product tagpap = new ProductPerMeterPrice(0, "ICOPAL SELVKLÆBER TOP SORT 1X5", "tagpap", 13980, 500, 100, 0);  //meterpris i Øre = 69900
         OrderLine result = calcTarPaper(length, width, tagpap, "fladt");
