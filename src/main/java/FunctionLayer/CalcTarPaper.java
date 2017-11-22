d7427fa85ed4bfad3dfbdc3f016d9c59e6e88413
Alexander W. Hørsted-Andersen <awha86@gmail.com>
2017-11-22 11:33:00 +0100

    update to tarPaper calc

--- src/main/java/FunctionLayer/CalcTarPaper.java
+++ src/main/java/FunctionLayer/CalcTarPaper.java
@@ -13,27 +13,22 @@ public class CalcTarPaper {  //milimeter
 
     public static OrderLine calcTarPaper(double length, double width, Product tarPaper) {
 
-        double productLength = tarPaper.getLength();
-        double productWidth = tarPaper.getWidth();
-        
-        double overlay = 100;
-        
-//        if(overlay * productWidth/1000 > )
-        
-        double tarPaperLenNeeded = length * ((width - overlay) / 10000);  //3,9 * 5000 = 19500
-        //sørge for at den runder korrekt op!
         int tarPaperCounter = 0;
+        double productLength = tarPaper.getLength();
+
+        double tarPaperLenNeeded = length * ((width) / 10000);  //pr meter width 
+
+        //tarPaper rolls needed
         while (tarPaperLenNeeded > 0) {
             tarPaperLenNeeded -= productLength;
             tarPaperCounter++;
         }
-
         return new OrderLine(tarPaper, tarPaperCounter);
     }
 
     public static void main(String[] args) {
-        double length = 6000;
-        double width = 5000;
+        double length = 2500;
+        double width = 4000;
         Product tagpap = new ProductPerMeterPrice(0, "ICOPAL SELVKLÆBER TOP SORT 1X5", "tagpap", 13980, 500, 100, 0);  //meterpris i Øre = 69900
         OrderLine result = calcTarPaper(length, width, tagpap);
         System.out.println(result.quantity);
