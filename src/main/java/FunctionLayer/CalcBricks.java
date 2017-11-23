af5ca0b9aa32e3829491975c283de05ed1c2e88d
mathias bigler <orchidevej@hotmail.com>
2017-11-23 15:59:14 +0100

    rygsten og tagsten done

---
+++ src/main/java/FunctionLayer/CalcBricks.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FunctionLayer;
+
+/**
+ *
+ * @author Orchi
+ */
+public class CalcBricks {
+    
+    public static OrderLine calculateAmountOfBricks(int length, int width, Product lath) {
+        int newWidth = lath.getWidth() - 20;
+        int newLength = lath.getLength() - 100;
+        
+        int rows = width / newLength;
+        rows++;
+        
+        int columns = length / newWidth;
+        columns++;
+        
+        int quantity = rows * columns;
+        
+        return new OrderLine(lath, length, quantity, "stk", "Monteres på taglægter fordelt på hver side af taget");
+    }
+    
+    public static OrderLine calculateAmountOfTopBricks(int length, int width, Product lath) {
+        int lengthMinusOverlap = lath.getLength() - 50;
+        int quantity = length / lengthMinusOverlap;
+        quantity++;
+        
+        return new OrderLine(lath, length, quantity, "stk", "monteres på toplægte med medfølgende beslag se " +
+                                                            "tagstens vejledning");
+    }
+}
