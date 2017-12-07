e36bdf8e6ac8d106de96d448bc2e6549ad4ad7e7
mathias bigler <orchidevej@hotmail.com>
2017-12-07 14:58:55 +0100

    developer merget med stringbuilder og exception

--- src/main/java/PresentationLayer/Calculate.java
+++ src/main/java/PresentationLayer/Calculate.java
@@ -22,6 +22,7 @@ public class Calculate extends Command {
 
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        request.getSession().setAttribute("lastpage", "QuickBuild");
         HttpSession session = request.getSession();
         int height = Integer.parseInt(request.getParameter("height"));
         int length = Integer.parseInt(request.getParameter("length"));
