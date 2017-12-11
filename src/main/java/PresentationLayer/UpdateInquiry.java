1e499f6fb4487d8a275ead3a7d297fba95c999f4
ML <ML@DESKTOP-8KPV0J8>
2017-12-11 14:33:10 +0100

    update inquiry

---
+++ src/main/java/PresentationLayer/UpdateInquiry.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentationLayer;
+
+import FunctionLayer.BillOfMaterials;
+import FunctionLayer.Calculator;
+import FunctionLayer.Inquiry;
+import FunctionLayer.LogicFacade;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ML
+ */
+public class UpdateInquiry extends Command {
+
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String customer = request.getParameter("customer");
+        int id = Integer.parseInt(request.getParameter("id"));
+        
+        int height = Integer.parseInt(request.getParameter("height"));
+        int length = Integer.parseInt(request.getParameter("length"));
+        int width = Integer.parseInt(request.getParameter("width"));
+        int shackLength = 0, shackWidth = 0;
+        if(request.getParameter("withShack").equals("ja")) {
+            shackLength = Integer.parseInt(request.getParameter("shackLength"));
+            shackWidth = Integer.parseInt(request.getParameter("shackWidth"));
+        }
+        String angle = null;
+        String roofType = request.getParameter("roofType");
+        if(roofType.equals("rejsning")){
+            angle = request.getParameter("angle");
+        }
+        String comment = request.getParameter("comment");
+        String status = request.getParameter("status");
+
+        Inquiry inquiry = LogicFacade.updateInquiry(id, height, length, width, shackLength, shackWidth, roofType, angle, comment, status);
+        BillOfMaterials bom = Calculator.getBillOfMaterials(inquiry);
+        
+        request.setAttribute("customer", LogicFacade.viewCustomerByEmail(customer));
+        request.setAttribute("bom", bom);
+        request.setAttribute("inquiry", inquiry);
+        
+        return "inquiry";
+    }
+    
+}
