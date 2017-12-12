85c6875e09396dea992e42ab1e1c09bf33fe43e9
ML <Mellem@MLs-MacBook-Pro.local>
2017-12-12 10:08:14 +0100

    DB script change. Ø -> OE

--- src/main/java/Diagrams/databaseScriptFog.sql
+++ src/main/java/Diagrams/databaseScriptFog.sql
@@ -144,7 +144,7 @@ INSERT INTO
 Inquiry (carportHeight,carportLength,carportWidth,shackWidth,shackLength,roofType,roofMaterial,angle,commentCustomer,commentEmployee,period, status, email)
 VALUES
 (320,420,320,200,120,'fladt','CEMBRIT OVENLYSPLADE B7 PVC GLASKLAR 1100X610X1MM',null,null,null,null,'gemt','test1@test.dk'),
-(320,420,320,0,0,'rejsning','RØDE VINGETAGSTEN GL. DANSK FORBRUG: 14,6 STK/M2','20','send Email',null,'2017-12-20','gemt','test1@test.dk'),
+(320,420,320,0,0,'rejsning','ROEDE VINGETAGSTEN GL. DANSK FORBRUG: 14,6 STK/M2','20','send Email',null,'2017-12-20','gemt','test1@test.dk'),
 (240,470,360,360,220,'fladt','CEMBRIT OVENLYSPLADE B7 PVC GLASKLAR 1100X610X1MM',null,'Kan der vælges andre tag-materialer end det viste?',null,'2017-12-24','ny','test2@test.dk'),
 (320,420,320,210,150,'rejsning','ROEDE VINGETAGSTEN GL. DANSK FORBRUG: 14,6 STK/M2','15','Hurtigts muligt','Ring op omkring leveringstidspunkt',null,'behandlet','test3@test.dk'),
 (210,420,320,null,null,'rejsning','ROEDE VINGETAGSTEN GL. DANSK FORBRUG: 14,6 STK/M2','15',null,null,null,'behandlet','test3@test.dk'),
@@ -253,7 +253,7 @@ VALUES
 
 -- trapeztag
 ('CEMBRIT OVENLYSPLADE B7 PVC GLASKLAR 1100X610X1MM','trapeztag', 24900, 1100, 610, 1),
-('CEMBRIT B6S FK GRÅ BØLGEPLADE 1090X1180MM - (MODEL 2013)','trapeztag', 25900, 1090, 1180, 1),
+('CEMBRIT B6S FK GRAA BOELGEPLADE 1090X1180MM - (MODEL 2013)','trapeztag', 25900, 1090, 1180, 1),
 
 -- løsholt
 ('45x95 Reglar ubh.','løsholt',7533,5400,95,45),
